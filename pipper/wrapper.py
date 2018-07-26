e5f7195772fb570002ab026181505304c90915a9
Scott Ernst <swernst@gmail.com>
2018-07-25 22:40:29 -0500

    Version 0.3.0
    Update pip wheel installation process to be compatible with changes in pip 18.0.

--- pipper/wrapper.py
+++ pipper/wrapper.py
@@ -1,19 +1,13 @@
-import pip
-import pkg_resources
+import subprocess
+import sys
 
-try:
-    # Starting with pip10 the `main` function has moved
-    # to the `_internal` module.
-    from pip import _internal as pip10
-except ImportError:
-    pip10 = None
+import pkg_resources
 
 from pipper import versioning
 
 
 def update_required(package_name: str, install_version: str) -> bool:
     """ """
-
     existing = status(package_name)
 
     if not existing:
@@ -32,7 +26,6 @@ def update_required(package_name: str, install_version: str) -> bool:
 
 def status(package_name: str):
     """ """
-
     try:
         return pkg_resources.get_distribution(package_name)
     except pkg_resources.DistributionNotFound:
@@ -42,16 +35,17 @@ def status(package_name: str):
 
 
 def install_wheel(wheel_path: str, to_user: bool = False):
-    """ """
-
-    cmd = ['install', wheel_path]
+    """
+    Installs the specified wheel using the pip associated with the
+    executing python.
+    """
+    cmd = [
+        sys.executable,
+        '-m', 'pip',
+        'install', wheel_path
+    ]
     cmd += ['--user'] if to_user else []
     print('COMMAND:', ' '.join(cmd))
 
-    # pip 10.0.0 moves the main function. Handle both 9.x and 10
-    # cases here for maximum compatibility.
-    execution_function = (
-        getattr(pip10, 'main', None)
-        or getattr(pip, 'main', None)
-    )
-    execution_function(cmd)
+    result = subprocess.run(cmd)
+    result.check_returncode()
